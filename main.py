adeeb9c65dfd0c1b2dddd4ba938a9d6d5fae3c6e
Brian Donohue <brian.donohue89@gmail.com>
2015-06-22 21:41:27 -0400

    Initial commit
    Signed-off-by: Brian Donohue <brian.donohue89@gmail.com>

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import json
+import requests
+from flask import Flask, request
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+SLACK_OUTGOING_WEBHOOK_TOKEN = os.environ.get('SLACK_OUTGOING_WEBHOOK_TOKEN', None)
+SLACK_INCOMING_WEBHOOK_URL = os.environ['SLACK_INCOMING_WEBHOOK_URL']
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    token = request.form.get('token')
+    if not SLACK_OUTGOING_WEBHOOK_TOKEN or token == SLACK_OUTGOING_WEBHOOK_TOKEN:
+        shamer = request.form.get('user_name')
+        shamee = request.form.get('text')
+        if not shamee.startswith('@'):
+            return 'Please enter a valid username to shame'
+
+        params = {
+                'username': 'shamebot',
+                'icon_emoji': ':bell:',
+                'text': '@%s shamed %s' % (shamer, shamee),
+                'channel': '#%s' % request.form.get('channel_name')
+        }
+        try:
+            requests.post(SLACK_INCOMING_WEBHOOK_URL, data=json.dumps(params))
+            params['channel'] = shamee
+            params['text'] = 'SHAME'
+            for i in range(3):
+                requests.post(SLACK_INCOMING_WEBHOOK_URL, data=json.dumps(params))
+        except Exception, e:
+            return 'Oops! Something went wrong: %s' % str(e)
+    return ''
+
